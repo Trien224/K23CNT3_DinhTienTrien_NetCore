1e8436269405d0d9424e4daecdb3681605937f8c
unknown <tientriendinh224@gmail.com>
2025-06-16 16:00:14 +0700

    lesson09

---
+++ Lesson09/Controllers/DttHomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Lesson09.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson09.Controllers
+{
+    public class DttHomeController : Controller
+    {
+        private readonly ILogger<DttHomeController> _logger;
+
+        public DttHomeController(ILogger<DttHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult DttIndex()
+        {
+            return View();
+        }
+
+        public IActionResult DttAoubt()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
